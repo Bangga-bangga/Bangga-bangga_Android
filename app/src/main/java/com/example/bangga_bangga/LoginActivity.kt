17c28e76a5fd49de2a46961ed4a6f09992a6c0da
yujeongkwon <87534067+yujeongkwon@users.noreply.github.com>
2023-12-08 05:15:01 +0900

    feat : 로그인 화면 UI

---
+++ app/src/main/java/com/example/bangga_bangga/LoginActivity.kt
@@ -0,0 +1,29 @@
+package com.example.bangga_bangga
+
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+
+class LoginActivity  : AppCompatActivity()  {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true) // 뒤로가기 버튼, 디폴트로 true만 해도 백버튼이 생김
+        supportActionBar?.title = ""
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> { //toolbar의 back키 눌렀을 때 동작
+                // 액티비티 이동
+                finish()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
