dc79b09186b1b333d508f96a9771d795859c246b
o-jeong <ljj020304@gmail.com>
2023-12-09 18:21:37 +0900

    [Feat#6] 배너 자동 스크롤 기능 추가

--- app/src/main/java/com/example/bangga_bangga/HomeActivity.kt
+++ app/src/main/java/com/example/bangga_bangga/HomeActivity.kt
@@ -1,25 +1,72 @@
 package com.example.bangga_bangga
 
+
 import android.os.Bundle
+
+import android.os.Message
 import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager2.widget.ViewPager2
 import com.example.bangga_bangga.databinding.ActivityHomeBinding
+//import java.util.logging.Handler
+import android.os.Handler
 
 class HomeActivity : AppCompatActivity() {
-//    private var numBanner = 10
     private var currentPosition = Int.MAX_VALUE / 2
-//    private var myHandler = MyHandler()
-//    private val intervalTime = 1500.toLong()  // 몇 초 간격으로 페이지를 넘길건지
+
+    private var myHandler = MyHandler()
+    private val intervalTime = 3500.toLong()  // 몇 초 간격으로 페이지를 넘길건지
+    private lateinit var homeBinding: ActivityHomeBinding // 바인딩 객체 선언
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val homeBinding = ActivityHomeBinding.inflate(layoutInflater)
+//        setContentView(R.layout.activity_home)
+        homeBinding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(homeBinding.root)
 
         homeBinding.viewPagerAdvice.adapter = BannerAdapter(getBannerList())
         homeBinding.viewPagerAdvice.orientation = ViewPager2.ORIENTATION_HORIZONTAL
-//        homeBinding.viewPagerAdvice.setCurrentItem(currentPosition, false)
+        homeBinding.viewPagerAdvice.setCurrentItem(currentPosition, false)
 
-
+        homeBinding.viewPagerAdvice.apply{
+            registerOnPageChangeCallback(object: ViewPager2.OnPageChangeCallback(){
+                override fun onPageScrollStateChanged(state: Int) {
+                    super.onPageScrollStateChanged(state)
+                    when (state) {
+                        ViewPager2.SCROLL_STATE_IDLE -> autoScrollStart(intervalTime)
+                        ViewPager2.SCROLL_STATE_DRAGGING -> autoScrollStop()
+                    }
+                }
+            })
+        }
+    }
+    private fun autoScrollStart(intervalTime: Long){
+        myHandler.removeMessages(0)
+        myHandler.sendEmptyMessageDelayed(0, intervalTime)
+    }
+    private fun autoScrollStop(){
+        myHandler.removeMessages(0) // 핸들러를 중지시킴
+    }
+    private inner class MyHandler : Handler() {
+        override fun handleMessage(msg: Message) {
+            super.handleMessage(msg)
+            if(msg.what == 0) {
+                homeBinding.viewPagerAdvice.setCurrentItem(++currentPosition, true) // 다음 페이지로 이동
+                autoScrollStart(intervalTime) // 스크롤을 계속 이어서 한다.
+            }
+        }
+    }
+    private fun incrementCurrentPosition() {
+        currentPosition++
+    }
+    // 다른 페이지 갔다가 돌아오면 다시 스크롤 시작
+    override fun onResume() {
+        super.onResume()
+        autoScrollStart(intervalTime)
+    }
+    // 다른 페이지로 떠나있는 동안 스크롤이 동작할 필요는 없음. 정지
+    override fun onPause() {
+        super.onPause()
+        autoScrollStop()
     }
     private fun getBannerList(): Array<Array<String>>{
         return arrayOf(
