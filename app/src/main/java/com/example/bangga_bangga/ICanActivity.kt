1a8f2e1e1bf26a2c40f00251d8776fb2b9a814b2
sunga <tjddk6662@naver.com>
2023-12-12 02:52:10 +0900

    feat : 메인에서 나는 할 수 있다 페이지 이동 기능 추가
    메인 액티비티에서 나는 할 수 있다 페이지로 갈 수 있도록 함
    
    issues : #28

---
+++ app/src/main/java/com/example/bangga_bangga/ICanActivity.kt
@@ -0,0 +1,11 @@
+package com.example.bangga_bangga
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ICanActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_i_can)
+    }
+}
