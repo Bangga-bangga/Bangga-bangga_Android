d96e7c761b69bb1f997951c2a7ed3ae5ec485911
sunga <tjddk6662@naver.com>
2023-12-12 03:30:55 +0900

    feat : 캐릭터 클릭하면 "나는 할 수 있다!!" 뜨게 하기
    나는 할 수 있다 페이지에서 캐릭터를 터치하면 나는 할 수 있다가 뜨고 손을 때면 사라짐
    
    issues : #28

--- app/src/main/java/com/example/bangga_bangga/ICanActivity.kt
+++ app/src/main/java/com/example/bangga_bangga/ICanActivity.kt
@@ -1,11 +1,39 @@
 package com.example.bangga_bangga
 
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class ICanActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_i_can)
+
+        var iCanCharacter = findViewById<ImageView>(R.id.i_can_character_img)
+        var touchCount = 0
+
+        iCanCharacter.setOnClickListener {
+            touchCount += 1
+        }
+
+        iCanCharacter.setOnTouchListener { v, event ->
+            handleTouch(event)
+        }
+    }
+
+    private fun handleTouch(event: MotionEvent): Boolean {
+        var iCanText = findViewById<TextView>(R.id.i_can_text)
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+                iCanText.visibility = View.VISIBLE
+            }
+            MotionEvent.ACTION_UP -> {
+                iCanText.visibility = View.INVISIBLE
+            }
+        }
+        return true
     }
 }
