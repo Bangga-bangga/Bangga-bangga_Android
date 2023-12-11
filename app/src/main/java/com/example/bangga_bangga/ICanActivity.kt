9102c2e40dbeb682486ad21e3ffd911d99e174de
sunga <tjddk6662@naver.com>
2023-12-12 04:26:19 +0900

    feat : 캐릭터 터치하면 캐릭터가 커지기
    나는 할 수 있다 페이지에서 캐릭터를 터치하면 캐릭터가 점점 커지기
    
    issues : #28

--- app/src/main/java/com/example/bangga_bangga/ICanActivity.kt
+++ app/src/main/java/com/example/bangga_bangga/ICanActivity.kt
@@ -1,23 +1,29 @@
 package com.example.bangga_bangga
 
 import android.os.Bundle
+import android.util.Log
 import android.view.MotionEvent
 import android.view.View
+import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class ICanActivity : AppCompatActivity() {
+    lateinit var iCanCharacter: ImageView
+    lateinit var iCanText: TextView
+    var touchCount = 0
+    var originalBottomMargin = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_i_can)
 
-        var iCanCharacter = findViewById<ImageView>(R.id.i_can_character_img)
-        var touchCount = 0
+        iCanCharacter = findViewById<ImageView>(R.id.i_can_character_img)
+        iCanText = findViewById<TextView>(R.id.i_can_text)
 
-        iCanCharacter.setOnClickListener {
-            touchCount += 1
-        }
+        val layoutParams = iCanCharacter.layoutParams as ViewGroup.MarginLayoutParams
+        originalBottomMargin = layoutParams.bottomMargin
 
         iCanCharacter.setOnTouchListener { v, event ->
             handleTouch(event)
@@ -25,15 +31,38 @@ class ICanActivity : AppCompatActivity() {
     }
 
     private fun handleTouch(event: MotionEvent): Boolean {
-        var iCanText = findViewById<TextView>(R.id.i_can_text)
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
                 iCanText.visibility = View.VISIBLE
+
+                touchCount += 1
+
+                val scale = 1f + 0.1f * touchCount
+                iCanCharacter.scaleX = scale
+                iCanCharacter.scaleY = scale
+
+                val layoutParams = iCanCharacter.layoutParams as ViewGroup.MarginLayoutParams
+                val newBottomMargin = (originalBottomMargin * scale - 100).toInt()
+
+                updateTextPosition(newBottomMargin)
+
+                updateCharacterBottomMargin(layoutParams, scale)
             }
             MotionEvent.ACTION_UP -> {
                 iCanText.visibility = View.INVISIBLE
             }
         }
         return true
+    }
+
+    private fun updateCharacterBottomMargin(layoutParams: ViewGroup.MarginLayoutParams, scale: Float) {
+        layoutParams.bottomMargin = (originalBottomMargin * scale).toInt()
+        iCanCharacter.layoutParams = layoutParams
+    }
+
+    private fun updateTextPosition(newBottomMargin: Int) {
+        val textLayoutParams = iCanText.layoutParams as ViewGroup.MarginLayoutParams
+        textLayoutParams.bottomMargin = newBottomMargin
+        iCanText.layoutParams = textLayoutParams
     }
 }
