78fb4b9e602a11a0c8fda6e14e6a3fd151401109
sunga <tjddk6662@naver.com>
2023-12-14 05:24:28 +0900

    style : 감정쓰레기통 키워드 입력 페이지 UI 수정
    애플리케이션 컨셉에 맞게 UI 수정
    
    issues : #7

--- app/src/main/java/com/example/bangga_bangga/TrashKeywordActivity.kt
+++ app/src/main/java/com/example/bangga_bangga/TrashKeywordActivity.kt
@@ -19,12 +19,12 @@ class TrashKeywordActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_trash_keyword)
 
-//        /** 툴바 생성 **/
-//        val toolbar: androidx.appcompat.widget.Toolbar = findViewById(R.id.toolbar)
-//        setSupportActionBar(toolbar)
-//        supportActionBar?.setDisplayHomeAsUpEnabled(true) // 뒤로가기 버튼, 디폴트로 true만 해도 백버튼이 생김
-//        supportActionBar?.title = ""
-//        /** 툴바 코드 끝 아래 onOptionsItemSelected또한 복사**/
+        /** 툴바 생성 **/
+        val toolbar: androidx.appcompat.widget.Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true) // 뒤로가기 버튼, 디폴트로 true만 해도 백버튼이 생김
+        supportActionBar?.title = ""
+        /** 툴바 코드 끝 아래 onOptionsItemSelected또한 복사**/
 
         val editTextEmotionKeyword = findViewById<EditText>(R.id.editText_emotion_keyword)
         val inputKeywordBtn = findViewById<Button>(R.id.input_keyword_btn)
@@ -41,7 +41,7 @@ class TrashKeywordActivity : AppCompatActivity() {
                 val keywordTextView = TextView(this)
                 keywordTextView.text = keyword
                 keywordTextView.setTextColor(Color.parseColor("#2A2500"))
-                keywordTextView.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 30f)
+                keywordTextView.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 25f)
                 keywordTextView.gravity = Gravity.CENTER
                 keywordTextView.setPadding(0, 15, 0, 15)
                 keywordListLayout.addView(keywordTextView)
@@ -63,16 +63,16 @@ class TrashKeywordActivity : AppCompatActivity() {
 
     }
 
-//    /** 툴바**/
-//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-//        when (item.itemId) {
-//            android.R.id.home -> {
-//                //toolbar의 back키 눌렀을 때 동작
-//                // 액티비티 이동
-//                finish()
-//                return true
-//            }
-//        }
-//        return super.onOptionsItemSelected(item)
-//    }
+    /** 툴바**/
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                //toolbar의 back키 눌렀을 때 동작
+                // 액티비티 이동
+                finish()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
 }
